4f0aeedc25873c804a731f2201de2a75ac67e3ff
Mateus <mateusmesquitalima18@gmail.com>
2026-06-23 23:36:40 -0300

    test

---
+++ src/manhunt/reverse/RenderWare.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <rwcore.h>
+#include <rwplcore.h>
+#include <rpworld.h>
+#include <rphanim.h>
+#include <rtcharse.h>
+#include <math.h>
+
+#include "../../../framework.h"
+#include "../../game.sdk.h"
