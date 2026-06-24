cebe9392b1e1aa58afe2c666a885bce759228adf
Mateus <mateusmesquitalima18@gmail.com>
2026-06-23 23:38:07 -0300

    Documentando os ids do menus + manuseio de menus

--- src/manhunt/ui/CMainMenu.cpp
+++ src/manhunt/ui/CMainMenu.cpp
@@ -17,10 +17,19 @@ int& selectedItemIndex = *reinterpret_cast<int*>(0x7C89CC);
 int& selectedOption = *reinterpret_cast<int*>(0x7C8A68);
 int& menu_mouse_in_area = *(int*)0x7C8F80;
 
+int& CurrentPlayerMenu = *reinterpret_cast<int*>(0x7C86F8);
+
 int& dword_7C89EC = *reinterpret_cast<int*>(0x7C89EC);
 int& dword_7C8A6C = *reinterpret_cast<int*>(0x7C8A6C);
 int& dword_7C86E8 = *reinterpret_cast<int*>(0x7C86E8);
 int& dword_7D6148 = *reinterpret_cast<int*>(0x7D6148);
+
+
+int& dword_7C86F0 = *reinterpret_cast<int*>(0x7C86F0);
+int& dword_7C86F4 = *reinterpret_cast<int*>(0x7C86F4);
+int& dword_7D8F58 = *reinterpret_cast<int*>(0x7D8F58);
+int* dword_7D7DC8 = *reinterpret_cast<int**>(0x7D7DC8);
+
 
 void*& off_7D614E = *reinterpret_cast<void**>(0x7D614E);
 void*& unk_7D6152 = *reinterpret_cast<void**>(0x7D6152);
@@ -29,8 +38,10 @@ void*& aUquit_1 = *reinterpret_cast<void**>(0x7D6156);
 typedef void (*sub_605030_t)(void*);
 sub_605030_t sub_605030 = (sub_605030_t)0x605030;
 
-typedef void* (__cdecl* tPrintCheat)(void* a1, void* a2, void* a3, void* a4);
-tPrintCheat oPrintCheat = (tPrintCheat)0x5D5BB0;
+
+typedef signed int (*tMainMenuHandler)();
+tMainMenuHandler original_MainMenuHandler = (tMainMenuHandler)0x600B34;
+SafetyHookInline g_MainHandlerMenuHook;
 
 SafetyHookInline g_MainMenuHook;
 safetyhook::InlineHook hook_selected, hook_normal;
@@ -41,17 +52,63 @@ SafetyHookInline g_PauseMenuHandlerHook;
 
 SafetyHookInline g_MainSubMenuHook;
 
-int& dword_7C89D4 = *reinterpret_cast<int*>(0x7C89D4);
+SafetyHookMid g_MenuProcesHook;
+
+
+DWORD& g_MenuOption = *reinterpret_cast<DWORD*>(0x7C89D4);
 int& dword_7C89E4 = *reinterpret_cast<int*>(0x7C89E4);
 
-int sub_606EB0(int* saveList, double a2, int a3)
+
+
+int sub_606EB0(int* this_, int a2)
 {
-    return CallAndReturn<int, 0x606EB0, int*, double, int>(saveList, a2, a3);
+    return CallAndReturn<int, 0x606EB0, int*, int>(this_, a2);
 }
 
+bool sub_5D87F0()
+{
+    return CallAndReturn<bool, 0x5D87F0>();
+}
+
+void sub_5DAA20()
+{
+    Call<0x5DAA20>();
+}
+
+int sub_5DAA50()
+{
+    return CallAndReturn<int, 0x5DAA50>();
+}
+
+int CMainMenu::getSelectionOption()
+{
+    return CallAndReturn<int, 0x5DAD00>();
+}
+
+void sub_5D6A60()
+{
+    Call<0x5D6A60>();
+}
+
+BYTE* sub_5D6720(int saveIndex, int a2)
+{
+    return CallAndReturn<BYTE*, 0x5D6720, int, int>(saveIndex, a2);
+}
+
+int ExistsSaveGame()
+{
+    return CallAndReturn<int, 0x5D6900>();
+}
+
+#include "../core/script/CSkinSelector.h"
+
+DWORD g_MaxMenuOptions = 0;
 
 void* __cdecl CMainMenu::hkMainMenu()
 {
+    
+    // ------------------------------------------------------------------
+
     int v0 = *(int*)0x7C8720;
     float v1 = *(float*)0x7C8724;
     float v2 = *(float*)0x7C8728;
@@ -61,19 +118,20 @@ void* __cdecl CMainMenu::hkMainMenu()
 
     int logoTexture = CVisual::LoadTextureTXD(*(int*)0x7C8704, "logo");
     float logoScale = *(float*)0x7D3458 * *(float*)0x7D63FC;
-    CVisual::DrawImage(*(float*)0x7D6408, *(float*)0x7D6404, logoScale, *(float*)0x7D6400, 180, 180, 180, 255, logoTexture);
+    CVisual::DrawColoredQuad(*(float*)0x7D6408, *(float*)0x7D6404, logoScale, *(float*)0x7D6400, 180, 180, 180, 255, logoTexture);
 
     float x = *(float*)0x7D6410;
     float baseY = *(float*)0x7D640C;
     float step = 0.06f;
+
 
     CVisual::DrawMenuItem(CText::GetKey("PLAY"), x, baseY + (step * 0), v0, v1, selected == 0);
     CVisual::DrawMenuItem(CText::GetKey("SELSCE"), x, baseY + (step * 1), v0, v1, selected == 1);
     CVisual::DrawMenuItem(CText::GetKey("LOADG"), x, baseY + (step * 2), v0, v1, selected == 2);
     CVisual::DrawMenuItem(CText::GetKey("SETT"), x, baseY + (step * 3), v0, v1, selected == 3);
     CVisual::DrawMenuItem(CText::GetKey("BONFEA"), x, baseY + (step * 4), v0, v1, selected == 4);
-    CVisual::DrawMenuItem(CText::GetKey("BONFEA"), x, baseY + (step * 4), v0, v1, selected == 4);
     CVisual::DrawMenuItem(CText::GetKey("QUITPRG"), x, baseY + (step * 5), v0, v1, selected == 5);
+    CVisual::DrawMenuItem(L"SKINS", x, baseY + (step * 7), v0, v1, selected == 6);
 
     int cheatFlag = *(int*)0x7C84A8;
     wchar_t* cheatText = (wchar_t*)0x7D6360;
@@ -95,11 +153,93 @@ void* __cdecl CMainMenu::hkMainMenu()
     }
 
     if (*(int*)0x7C84AC == 1) {
-        oPrintCheat(cheatText, (void*)0x7D6360, (void*)0x7D6360, (void*)0x7D6360);
+        CVisual::DrawMenuInfo(cheatText, (wchar_t*)0x7D6360, (wchar_t*)0x7D6360, (wchar_t*)0x7D6360);
     }
 
     return (void*)0x7D6360;
 }
+
+void PlayLevel(int level, int unk)
+{
+    Call<0x5D6720, int, int>(level, unk);
+}
+
+int __cdecl CMainMenu::hkMainMenuHandler()
+{
+    int ret = original_MainMenuHandler();
+    int v1; // eax
+
+    if (!sub_5D87F0())
+        return 1;
+
+    dword_7C89EC = 0;
+
+    if (!dword_7D8F58)
+        sub_606EB0(dword_7D7DC8, 1);
+
+    sub_5DAA20();
+    sub_5DAA50();
+
+    if (CInput::IsUpKeyPressed())
+    {
+        if (--g_MenuOption < 0)
+            g_MenuOption = (g_MaxMenuOptions-1);
+    }
+    if (CInput::IsDownKeyPressed())
+    {
+        if (++g_MenuOption >= g_MaxMenuOptions)
+            g_MenuOption = 0;
+    }
+    if (CInput::GetSelectedOptionFromMouse())
+        g_MenuOption = CMainMenu::getSelectionOption();
+    if (CInput::IsExitKeyPressed())
+        CPlayer::SetMenu(MENU_SAIR);
+    if (!CInput::IsActionKeyPressed())
+        return 1;
+
+    if (!g_MenuOption) // play
+    {
+        v1 = ExistsSaveGame();
+        if (v1 == -1)
+        {
+            sub_5D6A60();
+            dword_7C86F0 = 1;
+            dword_7C86F4 = 0;
+            CPlayer::SetMenu(MENU_BRILHO);
+        }
+        else
+        {
+            PlayLevel(v1, 1);
+        }
+    }
+    if (g_MenuOption == 1)  // SELECIONAR
+    {
+        dword_7C89E4 = 7;
+        CPlayer::SetMenu(MENU_SELECIONAR);
+    }
+    if (g_MenuOption == 2) // CARREGAR
+    {
+        CPlayer::SetMenu(MENU_CARREGAR);
+    }
+    if (g_MenuOption == 3) // CONFIG
+    {
+        CPlayer::SetMenu(MENU_CONFIG);
+    }
+    if (g_MenuOption == 4) // BONUS
+    {
+        CPlayer::SetMenu(MENU_BONUS);
+    }
+    if (g_MenuOption == 5) // SAIR
+    {
+        CPlayer::SetMenu(MENU_SAIR);
+    }
+    if (g_MenuOption == 6)
+    {
+        CPlayer::SetMenu(MENU_SKINS);
+    }
+    return ret;
+}
+
 
 void CMainMenu::SetColor(int r, int g, int b, int a)
 {
@@ -202,45 +342,6 @@ signed int CMainMenu::hkMainSubMenuHandler(void* thisPtr)
     return 1;
 }
 
-bool sub_5D87F0()
-{
-    return CallAndReturn<bool, 0x5D87F0>();
-}
-
-void sub_5DAA20()
-{
-    Call<0x5DAA20>();
-}
-
-int sub_5DAA50(int* a1)
-{
-    return CallAndReturn<int, 0x5DAA50, int*>(a1);
-}
-
-int sub_5DAD00()
-{
-    return CallAndReturn<int, 0x5DAD00>();
-}
-
-void sub_5D6A60()
-{
-    Call<0x5D6A60>();
-}
-
-BYTE* sub_5D6720(int saveIndex, int a2)
-{
-    return CallAndReturn<BYTE*, 0x5D6720, int, int>(saveIndex, a2);
-}
-
-int sub_5D6900()
-{
-    return CallAndReturn<int, 0x5D6900>();
-}
-
-int& dword_7C86F0 = *reinterpret_cast<int*>(0x7C86F0);
-int& dword_7C86F4 = *reinterpret_cast<int*>(0x7C86F4);
-int& dword_7D8F58 = *reinterpret_cast<int*>(0x7D8F58);
-int* g_SaveFileList = *reinterpret_cast<int**>(0x7D7DC8);
 
 SafetyHookInline g_VideoMenuHook;
 SafetyHookInline g_AudioMenuHook;
@@ -250,19 +351,41 @@ void* __cdecl hkVideoMenu() { return nullptr; }
 void* __cdecl hkAudioMenu() { return nullptr; }
 void* __cdecl hkControlsMenu() { return nullptr; }
 
-typedef signed int (*tMainMenu_HandleInput)(int*, double);
-tMainMenu_HandleInput original_MainMenu_HandleInput = (tMainMenu_HandleInput)0x600AF0;
 
 SafetyHookInline g_ModLoaderHook;
 
-int hkMainMenu_Handler(int* a1, double a2)
+void HookExecuteMenuProcess(SafetyHookContext& ctx)
 {
-    return original_MainMenu_HandleInput(a1, a2);
+	if (CurrentPlayerMenu == MENU_SKINS) // SKINS
+	{
+        CSkinSelector::SkinSelectorMenu();
+        CSkinSelector::SkinSelectorMenuHandler();
+    }
 }
 
 void CMainMenu::InstallHook()
 {
+    // configs
+    g_MaxMenuOptions = 7;
+
+    CSkinSelector::Initialise();
+
+    // hooks
     g_MainMenuHook = safetyhook::create_inline((void*)0x600C20, (void*)&CMainMenu::hkMainMenu);
+    
+    // MENUS
+    PATCH(0x600BCB, 0x90, 0x4B); // seleção de menu
+    PATCH(0x600B25, 0x90, 0x65); // navegação de menu
+    
+    g_MenuProcesHook = safetyhook::create_mid(
+        (void*)0x5D75D7,
+        HookExecuteMenuProcess
+    );
+
+    g_MainHandlerMenuHook = safetyhook::create_inline((void*)0x600AF0, (void*)&CMainMenu::hkMainMenuHandler);
+    original_MainMenuHandler = g_MainHandlerMenuHook.original<tMainMenuHandler>();
+
+    // SELEÇÃO
     hook_selected = safetyhook::create_inline((void*)0x5D7B10, (void*)&CMainMenu::SetSelectedMenuItemColor);
     hook_normal = safetyhook::create_inline((void*)0x5D7BC0, (void*)&CMainMenu::SetNormalMenuItemColor);
 
--- src/manhunt/ui/CMainMenu.h
+++ src/manhunt/ui/CMainMenu.h
@@ -31,13 +31,48 @@ extern int& selectedItemIndex;
 extern int& selectedOption;
 extern int& menu_mouse_in_area;
 
+extern DWORD& g_MenuOption;
+
+
+enum
+{
+    MENU_LANGUAGE = 1,
+    MENU_UNK = 2,
+    MENU_DIFICULTY = 3,
+    MENU_CARREGAR = 4,
+    MENU_LOADGAME = 5,
+    MENU_SAVEGAME = 6,
+    MENU_UNK2 = 7,
+    MENU_PAUSE = 8,
+    MENU_SELECIONAR = 9,
+    MENU_CONFIG = 10,
+    MENU_CONFIG_AUDIO = 11,
+    MENU_UNK3 = 12,
+    MENU_CONFIG_VIDEO = 12,
+    MENU_BONUS = 14,
+    MENU_BONUS_CLOWN = 15,
+    MENU_SCORE_FINAL = 16,
+    MENU_BRILHO = 17,
+    MENU_CONFIG_CONTROLS = 18,
+    MENU_CONFIG_CONTROLS_RESET = 19,
+    MENU_BUG = 20, // BLACK SCREEN
+    MENU_CREDITS = 21,
+    MENU_MAP_KEYBOARD = 22,
+    MENU_SAIR = 23,
+
+    MENU_SKINS = 24
+};
+
 class CMainMenu
 {
 private:
     static void CMainMenu::SetColor(int r, int g, int b, int a);
 
 public:
+    static int getSelectionOption();
+
     static void* __cdecl hkMainMenu();
+    static int __cdecl hkMainMenuHandler();
     static signed int hkPauseMenuHandler();
     static signed int hkMainSubMenuHandler(void* thisPtr);
     static void __stdcall SetSelectedMenuItemColor();
