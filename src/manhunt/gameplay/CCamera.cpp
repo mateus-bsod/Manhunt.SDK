088f23ff6c63f3c078fc91658ffb94f0b6b33216
Mateus <mateusmesquitalima18@gmail.com>
2026-07-01 11:41:04 -0300

    novas funções em CCamera

--- src/manhunt/gameplay/CCamera.cpp
+++ src/manhunt/gameplay/CCamera.cpp
@@ -10,6 +10,116 @@ float& g_CameraFlySensitivity = *reinterpret_cast<float*>(0x715AC4);
 // ModeCameraRec()
 // 
 
+// unused
+float* CCamera::Camera_GetBase(void) {
+    return (float*)CAMERA_BASE;
+}
+
+// A value is added to this function every frame; there isn't much use for it.
+float CCamera::Camera_GetNoSignedTimerCount(void) {
+    return *(float*)(CAMERA_BASE + 0x68);
+}
+
+void CCamera::Camera_SetNoSigned(bool state) {
+    *(bool*)(0x7A13DD) = state ? 1 : 0;
+}
+
+bool CCamera::Camera_GetNoSigned(void) {
+    return *(bool*)(0x7A13DD) != 0;
+}
+
+// If you set the value to zero, a "no signal" camera screen will appear for a few seconds—which is pretty useless; 
+// if you want better control, it is better to use SetCameraNoSigned.
+void CCamera::Camera_SetNoSignedTimerCount(float value) {
+    *(float*)(CAMERA_BASE + 0x68) = value;
+}
+
+// I believe it's something like a tremolo, but it likely operates on the same principle as the 
+// "Set/Get ZoomTemp" function, where the value returns to its initial state.
+float CCamera::Camera_GetTremolo(void) {
+    return *(float*)(CAMERA_BASE + 0x78);
+}
+
+void CCamera::Camera_SetTremolo(float value) {
+    *(float*)(CAMERA_BASE + 0x78) = value;
+}
+
+float CCamera::Camera_GetZoomTemp(void) {
+    return *(float*)(CAMERA_BASE + 0x7C);
+}
+
+void CCamera::Camera_SetZoomTemp(float value) {
+    *(float*)(CAMERA_BASE + 0x7C) = value;
+}
+
+float CCamera::Camera_GetLeftPosition(void) {
+    return *(float*)(CAMERA_BASE + 0x80);
+}
+
+void CCamera::Camera_SetLeftPosition(float value) {
+    *(float*)(CAMERA_BASE + 0x80) = value;
+}
+
+float CCamera::Camera_GetPosition(void) {
+    return *(float*)(CAMERA_BASE + 0x84);
+}
+
+void CCamera::Camera_SetPosition(float value) {
+    *(float*)(CAMERA_BASE + 0x84) = value;
+}
+
+float CCamera::Camera_GetAimPos(void) {
+    return *(float*)(CAMERA_BASE + 0xA8);
+}
+
+void CCamera::Camera_SetAimPos(float value) {
+    *(float*)(CAMERA_BASE + 0xA8) = value;
+}
+
+float CCamera::Camera_GetAimLeftPosition(void) {
+    return *(float*)(CAMERA_BASE + 0xAC);
+}
+
+void CCamera::Camera_SetAimLeftPosition(float value) {
+    *(float*)(CAMERA_BASE + 0xAC) = value;
+}
+
+// Front -> When you press F and the camera moves to the front of you.
+double CCamera::Camera_GetFrontRotation(void) {
+    return *(double*)(CAMERA_BASE + 0xB8);
+}
+
+void CCamera::Camera_SetFrontRotation(double value) {
+    *(double*)(CAMERA_BASE + 0xB8) = value;
+}
+
+float CCamera::Camera_GetFrontZoom(void) {
+    return *(float*)(CAMERA_BASE + 0xC0);
+}
+
+void CCamera::Camera_SetFrontZoom(float value) {
+    *(float*)(CAMERA_BASE + 0xC0) = value;
+}
+
+
+// Back -> rear camera, the standard one.
+double CCamera::Camera_GetBackPos(void) {
+    return *(double*)(CAMERA_BASE + 0xC8);
+}
+
+void CCamera::Camera_SetBackPos(double value) {
+    *(double*)(CAMERA_BASE + 0xC8) = value;
+}
+
+// unused
+char* CCamera::Camera_GetTCRString(void) {
+    return (char*)(CAMERA_BASE + 0x4A8);
+}
+
+void CCamera::Camera_SetTCRString(const char* str) {
+    strcpy((char*)(CAMERA_BASE + 0x4A8), str);
+}
+
 void CCamera::ToggleCameraFlyMode(bool state) {
     if (state)
     {
--- src/manhunt/gameplay/CCamera.h
+++ src/manhunt/gameplay/CCamera.h
@@ -7,8 +7,36 @@ extern int& g_Unk;
 extern float& g_CameraFlySpeed;
 extern float& g_CameraFlySensitivity;
 
+#define CAMERA_BASE 0x7A0FAC
+
 class CCamera {
 public:
+	static float* Camera_GetBase(void);
+	static float Camera_GetNoSignedTimerCount(void);
+	static void Camera_SetNoSigned(bool state);
+	static bool Camera_GetNoSigned(void);
+	static void Camera_SetNoSignedTimerCount(float value);
+	static float Camera_GetTremolo(void);
+	static void Camera_SetTremolo(float value);
+	static float Camera_GetZoomTemp(void);
+	static void Camera_SetZoomTemp(float value);
+	static float Camera_GetLeftPosition(void);
+	static void Camera_SetLeftPosition(float value);
+	static float Camera_GetPosition(void);
+	static void Camera_SetPosition(float value);
+	static float Camera_GetAimPos(void);
+	static void Camera_SetAimPos(float value);
+	static float Camera_GetAimLeftPosition(void);
+	static void Camera_SetAimLeftPosition(float value);
+	static double Camera_GetFrontRotation(void);
+	static void Camera_SetFrontRotation(double value);
+	static float Camera_GetFrontZoom(void);
+	static void Camera_SetFrontZoom(float value);
+	static double Camera_GetBackPos(void);
+	static void Camera_SetBackPos(double value);
+	static char* Camera_GetTCRString(void);
+	static void Camera_SetTCRString(const char* str);
+
 	static void ToggleCameraFlyMode(bool state);
 	static void SetCameraFlySpeed(float speed);
 	static void SetCameraFlySensitivity(float sensitivity);
